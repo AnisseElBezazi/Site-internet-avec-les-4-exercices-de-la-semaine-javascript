9f12aab40bacecb3d31ef5433a06f93752acbf0f
AnisseElBezazi <anisse.elbezazi@gmail.com>
2024-12-15 16:01:16 +0100

    Site avec exo semaine javascript
    Ajouts d'un dark mode, d'une page formulaire , Redesign de mon slider ,redesign de mes bouton ,responsive mobileL,M,S

---
+++ Anisse_ElBezazi_GR5 _Exercice_Tabs_et_simulation_de_combat_JavaScript/main2.js
@@ -0,0 +1,94 @@
+
+    class Guerrier{ 
+        constructor(attack,precision,vie,nom) 
+        {
+            this.attack = attack; 
+            this.precision = precision;
+            this.vie = vie; 
+            this.nom = nom;
+        }  
+    // getPrecision(){
+    // const luck= Math.random        je n'ai pas fait de deuxiem fonction pour la precision car sa ne sert a rien 
+    // return luck < this.precision}  mais si il y en avais une elle aurait ressembler a ca renvoyent true ou false 
+
+    getAttack(ennemie){ 
+        const luck= Math.random();
+     if (luck<this.precision) { /* si le math random est inferieur a la precision donné au personnage le personnage attaque */
+     ennemie.vie-= this.attack;/*enleve attaque au paramettre vie de l'ennemie*/
+     console.log (`${this.nom} a reussie son attaque sur ${ennemie.nom}pv lui infligent ${this.attack} point de degat`) ;
+     console.log ( ` il reste ${ennemie.vie} a ${ennemie.nom}`);
+    }  
+    else /*sinon le personnage rate son attaque */
+    console.log (`${this.nom} a raté son attaque il reste ${ennemie.vie}pv a ${ennemie.nom} `);
+    } 
+}
+var Leonard = new Guerrier(10,0.8,100,"Leonard"); 
+var Stuart = new Guerrier(20,0.4,100,"Stuart");
+
+while (Leonard.vie >0 && Stuart.vie >0) { /*tant que les deux on encore de la vie le combat continue */
+    console.log("=================new turn=====================")
+Leonard.getAttack(Stuart);
+Stuart.getAttack(Leonard);
+console.log("==========================================")
+}
+
+if  (Leonard.vie <=0 && Stuart.vie <=0){ /*Egalité */
+    console.log("======================================")
+    console.log (`LES DEUX COMBATTANT SONT MORT`);
+    console.log("======================================")
+}
+else if (Leonard.vie <= 0){
+    console.log("======================================")
+    console.log (`Stuart A GAGNER SON COMBAT `);
+    console.log("======================================")
+}   
+else{
+    console.log("======================================")
+    console.log (`Leonard A GAGNER SON COMBAT `);
+    console.log("======================================")
+}
+
+/*code pour exercice tabs*/
+let contenuList=document.querySelectorAll(".contenu")/*Je crée une Nodelist avec contenu1,2et3 de mon html*/
+let tabs=document.querySelectorAll(".tab")/*Je crée une Nodelist avec tab1,2et3 de mon html*/
+let tab1=document.querySelector(".tab1") /*Je crée une variable prenant seulement mon tab1 de mon html*/
+let tab2=document.querySelector(".tab2")/*Je crée une variable prenant seulement mon tab2 de mon html*/
+let tab3=document.querySelector(".tab3")/*Je crée une variable prenant seulement mon tab3 de mon html*/
+let contenu1=document.querySelector(".contenu1")/*Je crée une variable prenant seulement mon contenu1 de mon html*/
+let contenu2=document.querySelector(".contenu2")/*Je crée une variable prenant seulement mon contenu2 de mon html*/
+let contenu3=document.querySelector(".contenu3")/*Je crée une variable prenant seulement mon contenu3 de mon html*/
+
+    tab1.addEventListener ("click",function(){ /*ajout d'evenement lorsque mon menu 1 est cliquer */
+        tabs.forEach(function(tab){ /* réitère la function pour chaque element de ma Nodelist tabs */
+            tab.classList.remove('active') /*retire tout les menus --> retire la class active au tabs */
+        });
+        tab1.classList.add('active')/*ajoute la class active au tab1 */
+        contenuList.forEach(function(contenu){
+            contenu.classList.remove('contenu-active')/*retire tout contenu grace a un display none dans le css --> retire la class contenu-active aux contenu */
+        });
+        contenu1.classList.add('contenu-active');/*Affiche le menu 1 grace a un display block dans le css de contenu-active */
+       });
+
+
+       tab2.addEventListener ("click",function(){ /*ajout d'evenement lorsque mon menu 2 est cliquer */
+        tabs.forEach(function(tab){/* réitère la fonction pour chaque element de ma Nodelist tabs */
+            tab.classList.remove('active')/*retire tout les menus --> retire la class active au tabs */
+        });
+        tab2.classList.add('active') /*ajoute la class active au tab2 */
+        contenuList.forEach(function(contenu){/* réitère la function pour chaque element de ma Nodelist tabs */
+            contenu.classList.remove('contenu-active')/*retire tout contenu grace a un display none dans le css --> retire la class contenu-active aux contenu */
+        });
+        contenu2.classList.add('contenu-active')/*Affiche le menu 2 grace a un display block dans le css de contenu-active */
+
+       });
+
+       tab3.addEventListener ("click",function(){/*ajout d'evenement lorsque mon menu 3 est cliquer */
+        tabs.forEach(function(tab){ /* réitère la function pour chaque element de ma Nodelist tabs */
+            tab.classList.remove('active')/*retire tout les menus --> retire la class active au tabs */
+        });
+        tab3.classList.add('active')/*ajoute la class active au tab3 */
+        contenuList.forEach(function(contenu){ /* réitère la function pour chaque element de ma Nodelist contenuList */
+            contenu.classList.remove('contenu-active'); /*retire tout contenu grace a un display none dans le css --> retire la class contenu-active aux contenu */
+        });
+        contenu3.classList.add('contenu-active')/*Affiche le menu 3 grace a un display block dans le css de contenu-active */
+       });
---
+++ script.js
@@ -0,0 +1,104 @@
+/*Slider*/
+let indexcarte = 0; /* n'est pas vraiment affecter a une carte*/
+function slider_de_carte() {
+   const cartes= document.querySelectorAll('.carte'); /* creation d'une liste */
+    const totalCartes=cartes.length;    
+    const deplacement=-indexcarte *350 ;
+    document.querySelector('.cartes').style.transform = `translateX(${deplacement}px)`;
+
+    cartes.forEach((carte) => {
+        carte.classList.remove('active');
+    });
+        cartes[indexcarte+1].classList.add('active'); /* l'indexcarte est utilser ici pour naviguer dans la Nodeliste cartes*/
+        
+    }
+
+function moveSlide(direction){
+const cartes=document.querySelectorAll('.carte');
+  const totalCartes=cartes.length -1;
+    indexcarte= (indexcarte+direction+totalCartes)%totalCartes; //(calcul le reste, met a jour l'index pour avoir un slider circulaire  
+    slider_de_carte();
+}
+
+/*darkmode*/
+let variableConnexion= document.querySelector("#connexion"); /* creation d'une variable faisant appelle a mon id connexion dans le html*/
+let darkMode= document.querySelector("#dark");
+let back= document.querySelector("body");   /* creation d'une variable faisant appelle a mon body dans le html*/
+let titre= document.querySelector("#title");
+let backbutton= document.querySelector('#contenub');
+let menuvariable= document.querySelector('#mb');
+let liens= document.querySelectorAll('a') /* Creation d'une liste qui prend tout les liens present dans mon html*/
+darkMode.addEventListener ("click",function(){ /* execute une fonction lorsque mon id "dark" dans le html est cliquer*/
+    back.classList.toggle('bod');/*rajoute une class dans le html afin de modifier le css*/
+    titre.classList.toggle('Blue');
+    darkMode.classList.toggle('switchbutton');
+    backbutton.classList.toggle('whitebutton');
+    menuvariable.classList.toggle('Blue');
+    variableConnexion.classList.toggle('Blue'); 
+    
+    liens.forEach(function(lien){ /*Pour chaque lien de la liste soit les "a" selectioner precedament on va ajouter une class CSS */
+        lien.classList.toggle('Blue');
+   });
+   })
+
+/*formulaire*/
+   let form = document.querySelector("form");
+   let variableemail= document.querySelector("#email");
+   let variablePseudo= document.querySelector("#pseudo");
+   let variablePassword= document.querySelector("#password");
+   let variablePassword2= document.querySelector("#password2");
+   let ListeErreur = document.querySelector(".message-error ul");
+   let SuccesMessage =document.querySelector(".message-succes ul");
+form.addEventListener("submit",function(event){ 
+    ListeErreur.innerHTML ='';
+    SuccesMessage.innerHTML ='';
+    Valid = true ;
+    
+    if (variablePseudo.value.trim().length<6){  /*verifie si le pseudo a moins de 6 caracter */
+        variablePseudo.classList.remove("succes");
+        variablePseudo.classList.add("error");
+       ListeErreur.innerHTML += "<li>le pseudo doit comporter au moins 6 caractere</li>";
+    }
+    else{
+        variablePseudo.classList.remove("error");
+        variablePseudo.classList.add("succes");
+    }
+
+    if(variableemail.value.trim()===""){  /*verifie si le mail est vide */
+        variableemail.classList.remove("succes");
+        variableemail.classList.add("error");
+        ListeErreur.innerHTML += "<li>L'email est vide</li>";
+    }
+    else{
+        variableemail.classList.remove("error");
+        variableemail.classList.add("succes");
+    }
+
+    if (variablePassword.value.trim().length<8){  /*verifie si le mots de passe a au moins de 8 caracter */
+        variablePassword.classList.remove("succes");
+        variablePassword.classList.add("error");
+        ListeErreur.innerHTML += "<li>Votre mots de passe doit contenir au moins 8 caracter</li>";
+    }
+    else{
+        variablePassword.classList.remove("error");
+        variablePassword.classList.add("succes");
+    }
+
+    if (variablePassword.value.trim() !== variablePassword2.value.trim()){  /*verifie si le mots de passeest identique */
+        variablePassword2.classList.remove("succes");
+        variablePassword2.classList.add("error");
+        ListeErreur.innerHTML += "<li>Le mots de passe n'est pas identique</li>";
+    }
+    else{
+        variablePassword2.classList.remove("error");
+        variablePassword2.classList.add("succes");
+    }
+    if (variablePassword.value.trim() !==variablePassword2.value.trim() || variablePassword.value.trim().length<8 || variableemail.value.trim()==="" || variablePseudo.value.trim().length<6 ) {
+        Valid= false  /*verifie si une condition est fausse*/
+    }
+    
+    if (Valid==true) {
+        SuccesMessage.innerHTML += "<li>Le fomulaire a ete remplie avec succes !</li>"; /*Rajoute une ligne dans le html dans le <ul> de la division message-succes*/
+    }
+    event.preventDefault();
+})
